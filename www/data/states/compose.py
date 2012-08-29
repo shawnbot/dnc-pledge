eb23b8c4e13fee29a2b529fb072bcbc9fb79d72c
Shawn Allen <shawn@stamen.com>
2012-08-28 20:16:54 -0700

    new build of states GeoJSON

--- www/data/states/compose.py
+++ www/data/states/compose.py
@@ -8,7 +8,10 @@ collection = {
 
 for filename in sys.argv[1:]:
     fp = open(filename, 'r')
-    state = json.load(fp)['features'][0]
+    try:
+        state = json.load(fp)['features'][0]
+    except Exception, e:
+        print >> sys.stderr, "Choked on %s: %s" % (filename, e)
     # "USA-CA" -> "CA"
     abbr = state['id'][4:]
     state['id'] = state['properties']['abbr'] = abbr
